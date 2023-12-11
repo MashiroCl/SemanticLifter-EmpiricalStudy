4372d85ab614b3d2759394495b57e5513293922b
Lei Chen <mashirocl@gmail.com>
2023-12-11 17:31:54 +0100

    feat: collect within method explicitly annoatated "refactor/restruct" commit

--- collect_commit.py
+++ collect_commit.py
@@ -1,18 +1,118 @@
 import logging
 import pathlib
 import subprocess
+import re
 from config import load_config
+import json
 
 
 class Repository:
-    def __init__(self, name: str, path: pathlib.Path):
-        self.name = name
+    def __init__(self, path: pathlib.Path):
         self.path = path
 
 
-class RepositoryMethodLevel(Repository):
-    def __init__(self, name: str, path: pathlib.Path):
-        super().__init__(name, path)
+class CommitInfo:
+    def __init__(self, commitID: str, notes: str, message: list[str], change_status: list[str]):
+        self.commitID = commitID
+        self.notes = notes
+        self.message = message
+        self.change_status = change_status
+
+    def __str__(self):
+        return f'{self.commitID} - {self.notes}, {self.message} {self.change_status}'
+
+
+def is_commit_sha1(line: str):
+    pattern = re.compile(r'^[0-9a-fA-F]{40}$')
+    return bool(pattern.match(line.strip()))
+
+
+def is_commit_Notes(line: str, nextline: str) -> bool:
+    """
+    The Notes information in the method-level repo commit history
+    e.g.
+    Notes:
+    499ae3a4a3beb4c20de6d856a3eadeb7aaa0119b
+    :param line:
+    :return:
+    """
+    if "Notes:" in line and is_commit_sha1(nextline):
+        return True
+    return False
+
+
+def parse_commit_infoblock(infoblock: list[str]):
+    res = {}
+    if len(infoblock) < 1 or "commit" not in infoblock[0]:
+        raise ValueError("parse commit error, infoblock: \n", infoblock)
+    res["sha1"] = infoblock[0].split("commit ")[1]
+    res["message"] = []
+    res["files_status"] = []
+    isafterNotes = False
+    for i in range(1, len(infoblock)):
+        infoblock[i] = infoblock[i].strip()
+
+        # commit message
+        if "Author: " in infoblock[i] and "Date:   " in infoblock[i + 1]:
+            for j in range(i + 2, len(infoblock) - 1):
+                if is_commit_Notes(infoblock[j], infoblock[j + 1]):
+                    res["message"] = infoblock[i + 2:j - 1]
+                    i = j
+
+        # file status
+        if isafterNotes:
+            if len(infoblock[i]) > 0:
+                res["files_status"].append(infoblock[i])
+
+        # Notes
+        if is_commit_Notes(infoblock[i - 1], infoblock[i]):
+            res["Notes"] = infoblock[i].strip()
+            isafterNotes = True
+    return res
+
+
+def seperate_by_commit(commitInfos):
+    def is_commit_line(line, nextline):
+        if "commit" in line \
+                and len(line.split("commit ")) > 1 \
+                and is_commit_sha1(line.split("commit ")[1]) \
+                and "Author: " in nextline:
+            return True
+        return False
+
+    infoblocks = []
+
+    commitInfos = commitInfos.split("\n")
+    for i in range(len(commitInfos) - 1):
+        if is_commit_line(commitInfos[i], commitInfos[i + 1]):
+            for j in range(i + 1, len(commitInfos)):
+                if j == len(commitInfos) - 1 or is_commit_line(commitInfos[j], commitInfos[j + 1]):
+                    infoblocks.append(commitInfos[i:j])
+                    i = j
+    return infoblocks
+
+
+def git_log_with_name_status(git_path: pathlib.Path):
+    """
+    git log on the repository and parse the commit history into a list of CommitInfo objects
+    :param git_path: target method-level repository
+    :return:
+    """
+    logging.info("git log " + str(git_path))
+    command = f"git --git-dir {str(git_path)}/.git log --no-merges --name-status"
+    commitHistory = subprocess.getoutput(command)
+
+    logging.info("parsing commitInfos")
+    commitInfos = []
+    # seperate by commit
+    infoblocks = seperate_by_commit(commitHistory)
+    for infoblock in infoblocks:
+        parsed = parse_commit_infoblock(infoblock)
+        commitInfos.append(CommitInfo(parsed["sha1"], parsed["Notes"], parsed["message"], parsed["files_status"]))
+
+    logging.info(f"finished parsing {len(commitInfos)} commitInfos")
+
+    return commitInfos
 
 
 def to_method_level_files(repository: Repository, output_repository_path: pathlib.Path):
@@ -28,7 +128,71 @@ def to_method_level_files(repository: Repository, output_repository_path: pathli
     command = f"java -jar {git_stein_path}git-stein.jar {str(repository.path)} -o {str(output_repository_path)} @historage-jdt --no-original --no-classes --no-fields"
 
     logging.info(msg="running command: {}".format(command))
-    print(command)
     subprocess.getoutput(command)
     logging.info(msg="finished command: {}".format(command))
 
+
+def is_within_method_change(commitInfo: CommitInfo):
+    if len(commitInfo.change_status) == 1 and "M\t" in commitInfo.change_status[0]:
+        return True
+    return False
+
+
+def is_refactor_commit(commitInfo: CommitInfo):
+    if len(commitInfo.message) > 0 and (any("refactor" in each for each in commitInfo.message)
+                                         or any("restruct" in each for each in commitInfo.message)):
+        return True
+    return False
+
+
+def is_mjava(commitInfo: CommitInfo):
+    """
+    in the change status, the changed file is *.mjava
+    :param commitInfo:
+    :return:
+    """
+    if len(commitInfo.change_status) >0 and any(".mjava" in each for each in commitInfo.change_status):
+        return True
+    return False
+
+
+def commit_info_decoder(obj):
+    if obj.__class__ == CommitInfo.__class__:
+        return CommitInfo(obj['commitID'], obj['notes'], obj["message"], obj['change_status'])
+    return obj
+
+
+def commit_info_encoder(obj):
+    if isinstance(obj, CommitInfo):
+        return {
+            'commitID': obj.commitID,
+            'notes': obj.notes,
+            'message': obj.message,
+            'change_status': obj.change_status
+        }
+    return obj
+
+
+def extract_within_method_refactor_commit(repository_path, output_repository_path, within_method_refactor_json,
+                                          commitInfo_output_json=None):
+    repository_path = pathlib.Path(repository_path)
+    output_repository_path = pathlib.Path(output_repository_path)
+
+    to_method_level_files(Repository(repository_path), output_repository_path)
+    commitInfos = git_log_with_name_status(output_repository_path)
+
+    # write into json
+    if commitInfo_output_json:
+        with open(commitInfo_output_json, "w") as f:
+            json.dump(commitInfos, f, default=commit_info_encoder)
+
+    within_method_refactor = []
+
+    for commitInfo in commitInfos:
+        if is_within_method_change(commitInfo) and is_mjava(commitInfo) and is_refactor_commit(commitInfo):
+            within_method_refactor.append(commitInfo)
+
+    with open(within_method_refactor_json, "w") as f:
+        json.dump(within_method_refactor, f, default=commit_info_encoder)
+
+    logging.info(f"written {len(within_method_refactor)} into {within_method_refactor_json}")
--- test_collect.py
+++ test_collect.py
@@ -1,14 +1,80 @@
 import unittest
-from collect_commit import *
+# from collect_commit import *
+import pathlib
+from collect_commit import Repository, to_method_level_files, parse_commit_infoblock, git_log_with_name_status, extract_within_method_refactor_commit
 from config import load_config, logging_config
+
 
 
 class TestCollect(unittest.TestCase):
     def setUp(self):
         logging_config()
         self.config = load_config()
-        self.repository_mbassador = Repository('mbassador', pathlib.Path(self.config["mbassador"]))
+        self.repository_mbassador = Repository(pathlib.Path(self.config["mbassador"]))
         self.method_level_repository_output_path = pathlib.Path(self.config["method_level_repository_output_path"])
+        self.repository_presto = Repository(pathlib.Path(self.config["presto"]))
 
     def test_to_method_level_files_mbassador(self):
         to_method_level_files(self.repository_mbassador, pathlib.Path(str(self.method_level_repository_output_path)+"/mbassador_finergit"))
+
+
+    def test_parse_commit_infoblock(self):
+        infoblock = """commit c7d38a48fd4577103c53b82eea80452e4f6c8f71
+                    Author: Viacheslav Kolybelkin <kolybelkin@gmail.com>
+                    Date:   Thu Sep 26 13:42:18 2019 +0400
+                    
+                        Made it possible to extend MessagePublication class
+                    
+                    Notes:
+                        521ce6e6d96c238b14eb2e0c83e5ffadba8c3785
+                    
+                    """
+        res = parse_commit_infoblock(infoblock.split("\n"))
+        self.assertEqual(res["sha1"], "c7d38a48fd4577103c53b82eea80452e4f6c8f71")
+        self.assertEqual(res["Notes"], "521ce6e6d96c238b14eb2e0c83e5ffadba8c3785")
+        self.assertEqual(len(res["files_status"]), 0)
+
+    def test_parse_commit_infoblock2(self):
+        infoblock = """commit c0a1f84cef0ff7ba6cb80cd3b0498b3ff9d604d2
+                    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
+                    Date:   Tue Oct 13 06:50:00 2020 +0000
+                    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
+                    Date:   Tue Oct 13 06:50:00 2020 +0000
+                    
+                        Bump junit from 4.12 to 4.13.1
+                        
+                        Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
+                        - [Release notes](https://github.com/junit-team/junit4/releases)
+                        - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
+                        - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
+                        
+                        Signed-off-by: dependabot[bot] <support@github.com>
+                    
+                    Notes:
+                        499ae3a4a3beb4c20de6d856a3eadeb7aaa0119b
+                    
+                    M       pom.xml
+                    
+                    """
+        res = parse_commit_infoblock(infoblock.split("\n"))
+        self.assertEqual(res["sha1"], "c0a1f84cef0ff7ba6cb80cd3b0498b3ff9d604d2")
+        self.assertEqual(res["Notes"], "499ae3a4a3beb4c20de6d856a3eadeb7aaa0119b")
+        self.assertEqual(len(res["files_status"]), 1)
+
+
+    def test_git_log_with_name_status(self):
+        data = git_log_with_name_status(self.method_level_repository_output_path.joinpath("mbassador_finergit"))
+        self.assertEqual(len(data), 671)
+
+
+    def test_extract_within_method_change_commit_mbassador(self):
+        data = extract_within_method_refactor_commit(self.repository_mbassador.path,
+                                                   self.method_level_repository_output_path.joinpath("mbassador_finergit"),
+                                                   self.method_level_repository_output_path.joinpath("mbassador_withinmethod.json"))
+
+    def test_extract_within_method_change_commit_presto(self):
+            data = extract_within_method_refactor_commit(self.repository_presto.path,
+                                                         self.method_level_repository_output_path.joinpath(
+                                                             "presto_finergit"),
+                                                         self.method_level_repository_output_path.joinpath(
+                                                             "presto_withinmethod_refactor.json"))
